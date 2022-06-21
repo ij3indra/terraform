8650eec9f9a25ab84dc86c7938a355254fac55d3
Jatindra Suthar <jatindra.suthar@bharti-consulting-services.com>
2022-06-21 14:24:22 +0530

    op variables

--- practice/main.tf
+++ practice/main.tf
@@ -1,6 +1,6 @@
 resource "local_file" "pets" {
   filename = var.fileName
-  content  = var.content
+  content  = "Generated pet name is ${random_pet.my-pet.id}"
 }
 
 resource "random_pet" "my-pet" {
@@ -12,5 +12,13 @@ resource "random_pet" "my-pet" {
 resource "local_file" "map-file-content" {
   filename = "map-file.txt"
   content  = var.file-content["statement1"]
+
+  depends_on = [
+    random_pet.my-pet
+  ]
 }
- 
+
+output "pet-name" {
+  value       = random_pet.my-pet
+  description = "This is optional paramater to desctibe what is this op variable use for"
+}
